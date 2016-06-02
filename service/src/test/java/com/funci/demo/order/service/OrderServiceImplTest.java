5857e58c1bcdc15e0335e84324cb1f21e93606b3
lulu <lu_zhaohui@aliyun.com>
2016-06-02 13:47:21 +0800

    代码整理

--- service/src/test/java/com/funci/demo/order/service/OrderServiceImplTest.java
+++ service/src/test/java/com/funci/demo/order/service/OrderServiceImplTest.java
@@ -1,10 +1,14 @@
 package com.funci.demo.order.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:META-INF/spring/applicationContext*.xml")
 public class OrderServiceImplTest {
 	
 	private final static Logger logger = LoggerFactory.getLogger(OrderServiceImplTest.class);	
