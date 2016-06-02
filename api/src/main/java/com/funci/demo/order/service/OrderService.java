9ec175c950d824e434ad9eb47e2465725f24ddd7
lulu <lu_zhaohui@aliyun.com>
2016-06-02 13:45:55 +0800

    代码整理

--- api/src/main/java/com/funci/demo/order/service/OrderService.java
+++ api/src/main/java/com/funci/demo/order/service/OrderService.java
@@ -6,13 +6,26 @@ import java.util.Map;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Path;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.funci.demo.order.common.domain.Order;
 import com.funci.demo.order.common.domain.OrderExample;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Tag;
+
 @Path("order")
-//@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-//@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Api(value = "/order", description = "订单管理服务")
 public interface OrderService {
+	
+    @ApiOperation(value = "分页查找指定条件的订单", notes = "使用PageHelper进行分页查询", response = Tag.class, responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "符合条件的订单列表")})
+    @RequestMapping(value = "/{id}/list", method = RequestMethod.GET)	
+    public List<Order> getOrdersForPage(OrderExample example, int page, int size);
 	
 	List<Order> findAll();
 
@@ -29,7 +42,6 @@ public interface OrderService {
     public int getOrderCount(OrderExample example);
 
     public List<Order> getOrders(OrderExample example, int page, int size);
-    public List<Order> getOrdersForPage(OrderExample example, int page, int size);
 
     int updateOrderByOrder(Order order);
 
